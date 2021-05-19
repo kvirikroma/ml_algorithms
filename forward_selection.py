d53ecb0f4a4004ecc5f179bbe0f56b4c0aa0af45
crazylinuxer <kvirikroma@gmail.com>
2021-05-19 16:02:33 +0300

    finished forward_selection (i hope...)

--- forward_selection.py
+++ forward_selection.py
@@ -1,4 +1,4 @@
-from typing import Dict, Callable, Optional, Tuple, Union
+from typing import Dict, Callable, Optional, Tuple
 from multiprocessing import cpu_count
 from concurrent.futures import ProcessPoolExecutor
 from pandas import DataFrame, read_csv, get_dummies
@@ -6,6 +6,9 @@ from sklearn.neighbors import KNeighborsClassifier
 from matplotlib import pyplot as plt
 import numpy
 from sklearn import preprocessing
+import json
+
+from colors import yellow, green, blue
 
 
 def adapter(training_data_x: DataFrame, training_data_y: list, test_data_x: DataFrame, test_data_y: list) -> float:
@@ -36,12 +39,17 @@ def process_func(
 
 def forward_selection(
         training_data_x: DataFrame, training_data_y: list, test_data_x: DataFrame, test_data_y: list,
-        classification_algorithm: Callable[[DataFrame, list, DataFrame, list], float]
-) -> Dict[str, float]:
+        classification_algorithm: Callable[[DataFrame, list, DataFrame, list], float], max_steps: int = None
+) -> Dict[frozenset, float]:
     result: Dict[str, float] = {}
+    result_history: Dict[frozenset, float] = {}
     columns = list(training_data_x.columns)
     executor = ProcessPoolExecutor(max_workers=max(cpu_count() // 2, 1))
-    while len(result.keys()) < len(training_data_x.columns):
+    while len(result.keys()) < (
+            min(len(training_data_x.columns), max_steps)
+            if max_steps is not None
+            else len(training_data_x.columns)
+    ):
         mapped_rating = executor.map(
             process_func,
             (result for _ in columns),
@@ -59,57 +67,78 @@ def forward_selection(
             break
         maximum_accuracy = max(rating.keys())
         result[rating[maximum_accuracy]] = maximum_accuracy
+        result_history[frozenset(result.keys())] = maximum_accuracy
         print()
-        print(f"Calculated {len(result)} attributes of {len(training_data_x.columns)}")
-        print(result)
+        # print(f"{yellow('Rating')}: {rating}")
+        print(blue(f"Calculated {len(result)} attributes of {len(training_data_x.columns)}"))
+        print(f"{green('Current result')}: {result}")
     executor.shutdown()
-    return result
+    return result_history
 
 
-def prepare_data(data: DataFrame) -> DataFrame:
-    tmp_data = data.dropna("index", how='any')
+def prepare_data_part(data: DataFrame):
+    tmp_data = data.copy(deep=True)
     numeric_data = tmp_data.select_dtypes(include=('int16', 'int32', 'int64', 'float16', 'float32', 'float64'))
     non_numeric_data = frozenset(tmp_data.columns) - frozenset(numeric_data)
     non_numeric_data = {column: list(set(tmp_data[column].values)) for column in non_numeric_data}
     tmp_data = get_dummies(tmp_data, columns=non_numeric_data, drop_first=True)
-    values_scaled = preprocessing.MinMaxScaler().fit_transform(tmp_data.values)
+    return tmp_data.reindex(sorted(tmp_data.columns), axis=1)
+
+
+def prepare_data(data: DataFrame) -> DataFrame:
+    values_scaled = preprocessing.MinMaxScaler().fit_transform(data.values)
     final_data = DataFrame(values_scaled)
-    final_data.columns = [column.strip() for column in tmp_data.columns]
-    # final_data.columns = [str(i) for i in range(len(columns))]
-    return tmp_data
+    final_data.columns = [str(column).strip() for column in final_data.columns]
+    return final_data
 
 
 def main():
     training_data = read_csv(input("Введите расположение файла с данными для обучения: "))
     testing_data = read_csv(input("Введите расположение файла с тестовыми данными: "))
+    print()
     for data in training_data, testing_data:
-        data.columns = [column.strip() for column in data.columns]
         data.replace(' ?', numpy.nan, inplace=True)
         data.dropna("index", inplace=True, how='any')
+        data.columns = [column.strip() for column in data.columns]
+
+    training_data_y_name = list(training_data.columns)[-1]
+    training_data_y = list(training_data[training_data_y_name])
+    training_data = training_data.drop(training_data_y_name, axis=1)
+    testing_data_y_name = list(testing_data.columns)[-1]
+    testing_data_y = list(testing_data[testing_data_y_name])
+    testing_data = testing_data.drop(testing_data_y_name, axis=1)
+
+    all_values = list(
+        frozenset(str(item).strip('. ') for item in training_data_y).union(str(item).strip('. ') for item in testing_data_y)
+    )
+    all_values_dict = {all_values[i]: i for i in range(len(all_values))}
+    for data_y in training_data_y, testing_data_y:
+        for i in range(len(data_y)):
+            data_y[i] = all_values_dict[str(data_y[i]).strip('. ')]
+
+    testing_data = prepare_data_part(testing_data)
+    training_data = prepare_data_part(training_data)
     training_length = len(training_data)
     all_data = training_data.copy(deep=True)
     all_data = all_data.append(testing_data)
     all_data = prepare_data(all_data)
+    all_data.columns = training_data.columns
+    all_data.dropna(axis=1, inplace=True)
     training_data = all_data.iloc[:training_length, :]
     testing_data = all_data.iloc[training_length:, :]
-    training_data_y_name = list(training_data.columns)[-1]
-    training_data_y = training_data[training_data_y_name]
-    training_data = training_data.drop(training_data_y_name, axis=1)
-    testing_data_y_name = list(testing_data.columns)[-1]
-    testing_data_y = testing_data[testing_data_y_name]
-    testing_data = testing_data.drop(testing_data_y_name, axis=1)
-    weights = forward_selection(training_data, list(training_data_y), testing_data, list(testing_data_y), adapter)
-    print(f"\nResult: {weights}")
+    testing_data.columns = all_data.columns
+    training_data.columns = all_data.columns
+
+    weights_history = forward_selection(
+        training_data, list(training_data_y), testing_data, list(testing_data_y), adapter
+    )
     plt.figure(figsize=(25, 8))
-    sorted_columns = [*sorted(weights.keys(), key=lambda x: weights[x], reverse=True)]
-    columns_to_draw = [
-        f'{sorted_columns[i]}_{i}' if
-        sorted_columns[i].count('_') == 0 else
-        f'{sorted_columns[i][0]}{sorted_columns[i].split("_")[1].strip()[:1]}_{i}'
-        for i in range(len(sorted_columns))
-    ]
-    values_to_draw = [*sorted(weights.values(), reverse=True)]
+    sorted_columns = [*sorted(weights_history.keys(), key=lambda x: -len(x), reverse=True)]
+    columns_to_draw = [*range(len(sorted_columns))]
+    values_to_draw = [weights_history[i] for i in sorted_columns]
     plt.xticks(range(len(columns_to_draw)), columns_to_draw)
+    with open("./data/selection_result.json", 'w') as file:
+        json.dump({"data": [{"attributes": list(i), "accuracy": weights_history[i]} for i in sorted_columns]}, file, indent=2)
     plt.plot(columns_to_draw, values_to_draw)
     plt.show()
 
